4e403260c8109b12868a2f49f64327806e058375
Papa Ibrahima Diop <searchmethere@gmail.com>
2026-06-02 07:55:20 +0000

    Ajout de l'app gestionnaire des models et synch des apps, settings et BD

---
+++ src/core/admin.py
@@ -0,0 +1,70 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import (
+    Utilisateur, Classe, Administrateur, Enseignant,
+    Etudiant, Module, Affectation, Ressource, Annonce
+)
+
+# 1. Configuration des profils utilisateur
+@admin.register(Utilisateur)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'nom', 'prenom', 'role', 'get_profil_nce')
+    list_filter = ('role', 'is_staff')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informations Complémentaires', {'fields': ('role', 'telephone')}),
+    )
+
+    def get_profil_nce(self, obj):
+        # Utilisation d'un accès sécurisé pour éviter l'exception
+        if obj.role == 'ETUDIANT' and hasattr(obj, 'profil_etudiant'):
+            return obj.profil_etudiant.NCE
+        return "-"
+    get_profil_nce.short_description = 'NCE Etudiant'
+
+# 2. Gestion des Classes et Enseignants
+@admin.register(Classe)
+class ClasseAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'annee', 'get_effectif')
+    search_fields = ('nom',)
+
+    def get_effectif(self, obj):
+        return obj.etudiant_set.count()
+    get_effectif.short_description = 'Effectif'
+
+@admin.register(Affectation)
+class AffectationAdmin(admin.ModelAdmin):
+    list_display = ('enseignant', 'classe', 'module', 'est_responsable')
+    list_filter = ('classe', 'est_responsable')
+    list_select_related = ('enseignant', 'classe', 'module')
+
+# 3. Gestion des ressources pédagogiques
+@admin.register(Ressource)
+class RessourceAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'module', 'enseignant', 'date_ajout')
+    list_filter = ('module__classe', 'date_ajout')
+    search_fields = ('titre', 'description')
+    date_hierarchy = 'date_ajout'
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('intitule', 'classe')
+    list_filter = ('classe',)
+
+# 4. Enregistrement des autres modèles
+@admin.register(Administrateur)
+class AdministrateurAdmin(admin.ModelAdmin):
+    list_display = ('utilisateur',)
+
+@admin.register(Enseignant)
+class EnseignantAdmin(admin.ModelAdmin):
+    list_display = ('__str__',)
+
+@admin.register(Etudiant)
+class EtudiantAdmin(admin.ModelAdmin):
+    list_display = ('utilisateur', 'NCE', 'classe')
+    search_fields = ('NCE', 'utilisateur__nom')
+
+@admin.register(Annonce)
+class AnnonceAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'classe', 'enseignant', 'date_datetime')
+    list_filter = ('classe', 'date_datetime')
